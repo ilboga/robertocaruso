aac4b759c6ca284f59ea7b53ac23b555a9f644cc
Michael Bogani <michaelbogani@Michaels-MacBook-Pro.local>
2021-01-05 16:53:06 +0100

    swiper

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,30 @@
-export default {}
+import { Swiper, SwiperSlide, directive } from 'vue-awesome-swiper'
+import '~/node_modules/swiper/swiper.min.css'
+ 
+export default {
+  components: {
+    Swiper,
+    SwiperSlide
+  },
+  directives: {
+    swiper: directive
+  },
+  data() {
+    return {
+      banners: [ 
+          {url: require('~/assets/images/lavoro.jpeg'), alt: 'La prima img'},
+          {url: require('~/assets/images/piastrellatura.jpeg'), alt: 'La prima img'},
+          {url: require('~/assets/images/piastrellatura_2.jpeg'), alt: 'La prima img'},
+          {url: require('~/assets/images/tetto.jpeg'), alt: 'La prima img'}
+        ],
+      swiperOptions: {
+        autoplay: {
+          delay: 2000,
+        },
+        pagination: {
+          el: '.swiper-pagination'
+        }        
+      }
+    }
+  }
+}
